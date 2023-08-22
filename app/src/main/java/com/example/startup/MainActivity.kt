9901743936d562d4108d50d36302014eb86504cc
pudyasta <pudyastasatria@gmail.com>
2023-08-22 09:57:36 +0700

    feat(main activity)

--- app/src/main/java/com/example/startup/MainActivity.kt
+++ app/src/main/java/com/example/startup/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.startup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.example.startup.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val text = binding.nama.text
+        binding.signupButton.setOnClickListener { view ->
+            Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+        }
+        setContentView(binding.root)
     }
 }
